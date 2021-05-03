97c38d1279528a6fd790d3c2637fb1118e4d83d5
atul <atulpatare99@gmail.com>
2021-05-03 17:37:09 +0530

    model class added

---
+++ src/app/src/main/java/com/atul/musicplayerlite/model/Album.java
@@ -0,0 +1,11 @@
+package com.atul.musicplayerlite.model;
+
+import java.util.List;
+
+public class Album {
+    private String title;
+    private String year;
+
+    private Long duration;
+    private List<Music> music;
+}
